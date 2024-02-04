d32b6170538c68318b6b57c796da678a4c1f1f05
Peter Beaucage <github@peterbeaucage.com>
2024-02-03 19:38:14 -0500

    Add setup script, config scheme, camera support

--- afl-watchdog.py
+++ afl-watchdog.py
@@ -7,18 +7,58 @@ import os
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
 
-slack_token = os.environ['SLACK_TOKEN']
+import datetime
+import json
+
+config = json.load(open('~/.afl/watchdog-config.json','r'))
+slack_token = config['slack_token']
+slack_channel = config['slack_channel'] 
+slack_shutoff_msg = config['slack_shutoff_msg']
+try:
+    afl_system_serial = os.environ['AFL_SYSTEM_SERIAL']
+except KeyError:
+    afl_system_serial = config['afl_system_serial']
+stuck_n_checks = config['stuck_n_checks']
+idle_n_checks = config['idle_n_checks']
+servers = config['servers']
+delay = config['delay']
+image_endpoints = config['image_endpoints']
+upload_endpoint = config['upload_endpoint']
+
+
+# Create a SlackClient
 client = WebClient(token=slack_token)
 
 
 from AFL.automation.APIServer.Client import Client
 
-afl_system_serial = os.environ['AFL_SYSTEM_SERIAL']
 
 def send_alert(text):
+    '''
+    Send a message to the slack channel, if not muted.
+    '''
     alert_text = f"<!channel> {afl_system_serial}: {text}"
-    print(f'    --> Sent alert to Slack {alert_text}')
-    response = client.chat_postMessage(channel="C06F702K8HF",text=alert_text)
+    if not is_muted():
+        print(f'    --> Sent alert to Slack {alert_text}')
+        response = client.chat_postMessage(channel=slack_channel,text=alert_text)
+    else:
+        print(f'    --> Muted, did not send alert to Slack {alert_text}')
+
+def is_muted():
+    '''
+    check the slack channel for a message that says "SHUT UP"
+     (or a more polite equivalent from config).
+      If such a message exists, return True, else False.
+    '''
+    try:
+        response = client.conversations_history(channel=slack_channel)
+    except SlackApiError as e:
+        print(f"Error: {e}")
+        return False
+    for message in response['messages']:
+        if message['text'] == slack_shutoff_msg:
+            return True
+    return False
 
 '''
 server input schema:
@@ -34,23 +74,21 @@ server input schema:
                         'server_idle_timeout': warn if the server has been idle for the last 5 checks
                         'server_paused': warn if the server is paused
                         'server_stuck': warn if the server has been running the same task for the last 5 checks
-      'friendly_name': 'Test Server'
+      'friendly_name': 'Test Server',
+      'collect_status': True
     },
 '''
 
-stuck_n_checks = 5
-idle_n_checks = 5
-servers = [
-    { 'address': 'localhost',
-      'port':'5051',
-      'suppress_alerts': [],
-      'friendly_name': 'Test Server'
-    },
 
-]
-delay = 60
+
 
 while True:
+    status_dict = {}
+    status_dict['last_updated'] = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    if len(image_endpoints)>0:
+        status_dict['cameras'] = {}
+        for camera,endpoint in image_endpoints.items():
+            status_dict['cameras'][endpoint[0]] = endpoint[1]
     for server in servers:
         if 'port' not in server.keys():
             server['port'] = '5000'
@@ -64,8 +102,16 @@ while True:
             server['last_task_n_checks'] = 0
         if 'running_task_n_checks' not in server.keys():
             server['running_task_n_checks'] = 0
+        if 'collect_status' not in server.keys():
+            server['collect_status'] = True
+        
+        if server['collect_status']:
+            status_dict[server['friendly_name']] = {}
+            server_status = status_dict[server['friendly_name']]
+        else:
+            server_status = {}
 
-        print(f'Performing check on {server["friendly_name"]}')
+        print(f'--> Performing check on {server["friendly_name"]}')
 
         c = Client(server['address'],port=server['port'])
             
@@ -75,21 +121,28 @@ while True:
         except Exception:
             if 'server_down' not in server['suppress_alerts']:
                 send_alert(f'{server["friendly_name"]}: SERVER UNREACHABLE')
+                server_status['reachable'] = False
             continue
-    
+        server_status['reachable'] = True
+
         # check for server_paused
         state = c.queue_state().content.decode(encoding='utf-8')
         
+        server_status['state'] = state
+        server_status['errors'] = ''
         if state == 'Paused' and 'server_paused' not in server['suppress_alerts']:
             send_alert(f'{server["friendly_name"]}: SERVER PAUSED')
-        
+            server_status['errors'] += 'Server Paused | '
         queue = c.get_queue()
         
+        server_status['queue'] = queue
+        server_status['driver_status'] = c.driver_status()
+
         # check last task completion status
         if state == 'Paused' and 'server_paused_error' not in server['suppress_alerts']:
             if len(queue[1])>0:
                 send_alert(f'{server["friendly_name"]}: SERVER PAUSED W/ RUNNING TASK (likely error)')
-       
+                server_status['errors'] += 'Server Paused w/ Running Task | '
  
         if len(queue[1])>0:
             running_task_uuid = queue[1][0]['uuid']
@@ -115,8 +168,17 @@ while True:
 
         if server['last_task_n_checks'] >= idle_n_checks and 'server_idle_timeout' not in server['suppress_alerts']:
             send_alert(f'{server["friendly_name"]} SERVER APPEARS IDLE, has not run for last {server["last_task_n_checks"]} checks')
+            server_status['errors'] += 'Server Idle | '
         if server['running_task_n_checks'] >= stuck_n_checks and 'server_stuck' not in server['suppress_alerts']:
             send_alert(f'{server["friendly_name"]} SERVER APPEARS STUCK running for last {server["last_task_n_checks"]} checks')
- 
+            server_status['errors'] += 'Server Stuck | '
+    json.dump(status_dict,open('~/.afl/upload/status.json','w'))
+    print(f'--> fetching images for status board')
+    for fname,endpoint in image_endpoints.items():
+        print(f'    --> getting {fname} from {endpoint}')
+        os.system(f'wget -O ~/.afl/upload/{fname} {endpoint}')
+    if len(upload_endpoint)>0:
+        print(f'--> performing SSH upload of status directory')
+        os.system(f'scp -r ~/.afl/upload/* {upload_endpoint}')
     print(f'now sleeping for {delay}s')
     time.sleep(delay)    
---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mkdir -p ~/.afl/upload/
+
+cp ./dashboard2.html ~/.afl/upload/
+cp ./index.html ~/.afl/upload/
+
+cp -n ./config.json ~/.afl/watchdog-config.json
+
+
